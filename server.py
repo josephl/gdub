742005f17a2da5326a4ba8079c511f5ba4f6b45e
Joseph Lee <joseph@idealist.org>
2013-04-30 15:06:50 -0700

    Updated graphyte, whisperctl
    Signed-off-by: Joseph Lee <joseph@idealist.org>

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, session, jsonify, render_template, g
-from whisperctl import WhisperCtl
+import whisperctl
 import graphyte
 import metrics
 import logging
+from sys import argv
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app.config.from_pyfile(argv[1])
 app.secret_key = 'IDEALIST'
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -19,24 +20,29 @@ def getParams():
 
 @app.route('/graphite/', methods=['GET'])
 def index():
-    session.update({ 'metrics': metrics.metrics() })
+    g.update({ 'metrics': metrics.metrics() })
     return render_template('index.html')
 
 @app.route('/data', methods=['GET'])
 def data():
+    """Request metric data."""
     params = request.values.to_dict()
     if params.has_key('summarization'):
         summarization = int(params.pop('summarization'))
     else:
-        summarization = session['graph'].step
+        summarization = g.graph.step
     updateGraph(params)
     # BUG if timespan > 58hours, JSON req error. ?
-    if summarization > session['graph'].step:
-        table = session['graph'].timeTable(
-            session['graph'].summarize(summarization))
+    if summarization > g.graph.step:
+        table = g.graph.timeTable(
+            g.graph.summarize(summarization))
+    elif summarization == g.graph.step:
+        logger.info('step == summary')
+        table = g.graph.timeTable(
+            g.graph.summarize(summarization))
     else:
         logger.info('step >= summary')
-        table = session['graph'].timeTable()
+        table = g.graph.timeTable()
     logger.info('table length: %d' %(len(table)))
     try:
         jsonData = jsonify(results=table)
@@ -44,32 +50,37 @@ def data():
         logger.error('data request jsonification')
     return jsonData
 
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    params = request.values.to_dict()
+    
+
 @app.route('/graphite/<path:metricPath>', methods=['GET'])
 def graphite(metricPath):
     metricName = metricPath.rstrip('/').replace('/', '.')
     wc = WhisperCtl()
     metrics = wc.findall(metricName)
     if len(metrics) > 0:
-        session.update({ 'metrics': metrics })
+        g.metrics = metrics
         return render_template('index.html')
     return str(metrics)
 
 @app.route('/search', methods=['GET'])
 def search():
+    """Obtain metrics from Graphite"""
     return None
 
 def updateGraph(params):
-    if session.has_key('graph'):
-        if not session['graph'].has_key('target'):
-            session.pop('graph')
-        elif session['graph']['target'] != params['target']:
-            session.pop('graph')
+    if hasattr(g, 'graph') and g.graph is not None:
+        if not g.graph.has_key('target'):
+            g.graph = None
+        elif g.graph['target'] != params['target']:
+            g.graph = None
         else:
             # same metric, update instead of new request
             logger.info('same metric')
     # fresh request
-    session.update({ 'graph': graphyte.Graphyte(params) })
-    session['graph'].request()
+    setattr(g, 'graph', graphyte.request(params))
 
 
 if __name__ == '__main__':
